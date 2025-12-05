4738e2aa002f45a9929b4e88dd78c68c80998fab
vsattiraju <vasukisattiraju15@gmail.com>
2025-12-04 20:56:45 -0500

    Update Test.js

--- Test.js
+++ Test.js
@@ -1,4 +1,18 @@
+/*
 console.log ("Hello Vasuki")
 console.log ("I am learning Java script")
 console.warn ("VS")
 console.error("Error 400")
+*/
+
+console.log ("This is Vasuki Sattiraju")
+console.log ("Learning Java Script for TestComplete")
+console.log ("This is a testcomplete project")
+console.warn ("Warning message for Java script")
+console.log ("Success: 200")
+console.log ("Get Flight ticket to India")
+console.warn ("Only 4 tickets left")
+console.error ("Something Went Wrong")
+console.error ("400 Bad Request")
+
+
